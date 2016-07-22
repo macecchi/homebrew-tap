defd255892044394ee02f9d813a2b4e5264c90e8
Mario Cecchi <macecchi@gmail.com>
2016-07-22 15:59:20 -0300

    Update `xcopen` formula

--- xcopen.rb
+++ xcopen.rb
@@ -1,9 +1,9 @@
 class Xcopen < Formula
   desc "A shortcut to open the Xcode workspace or project in the current directory"
   homepage "https://github.com/macecchi/xcopen"
-  url "https://github.com/macecchi/xcopen/archive/0.2.tar.gz"
-  version "0.2"
-  sha256 "f1596c2869e2efc7951d0de0f0b26dadc81526369906b101ad2c542a2f33a576"
+  url "https://github.com/macecchi/xcopen/archive/0.3.tar.gz"
+  version "0.3"
+  sha256 "d095845359a6a39ae3570030298ddbe1daa57e0f8d1aa1bbc29a69dad3fcea6f"
 
   def install
     bin.install "xcopen"
